5302dedb6435281ac9c40bd467f711a89995009c
Tim Squires <tim@crystalmark.co.uk>
2022-04-25 08:10:58 +0100

    CTCP-308 initial tis prototype

---
+++ test/BuildSpec.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import scala.language.postfixOps
+
+import scala.sys.process._
+
+class BuildSpec extends AnyWordSpecLike with Matchers {
+  "Building the content" should {
+    "produce static files" in {
+      val result = "bundle install" #&& Process("bundle exec middleman build --verbose --build-dir=public/ --clean", None, "BASE_PATH" -> "/guides/ctc-traders-phase5-tis/") !
+
+      result shouldBe 0
+    }
+  }
+}
