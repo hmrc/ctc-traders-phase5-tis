7d496cf4b5f2d41e335e2ed5a66494fb439b186f
Tim Squires <tim@crystalmark.co.uk>
2022-08-04 19:53:25 +0100

    CTCP-590 loading rules from csv

--- python/rules.py
+++ python/rules.py
@@ -1,82 +1,21 @@
-import bs4 as bs
-import re
-import json
+import csv
+from html import escape
 
-def findTextUntil(startTag, endRegex):
-    s = ''
-    tag = startTag
-
-    while tag is not None:
-        
-
-        if re.match(endRegex, tag.text):
-            s = s+tag.text.replace("Functional Description", "")
-            if (len(s) != 0):
-                s = s.replace("IF", "  \r\n  \r\nIF")
-            s = s.replace("*","&#42;")
-            s = s.replace('<', "&lt;").replace('>', "&gt;").strip()
-            return s
-        else:
-            s = s + tag.text
-            s = s + "  \r\n"
-            tag = tag.findNext('p')
-
-    return s
-
-def findFuncHeaderTag(startTag, endRegex):
-    tag = startTag
-    while tag is not None:
-        if re.match(endRegex, tag.text):
-            return tag
-        tag = tag.findNext('p')
-
-def removeHeader(s):
-    return s.split(":")[1]
-
-def getIssueDate(soup):
-    p = soup.find('p', text=re.compile("Issue Date:(.*)"))
-    return p.text.replace("Issue Date:", '').strip()
-
-def getVersion(soup):
-    p = soup.find('p', text=re.compile("Release:(.*)"))
-    return p.text.replace("Release:", '').strip()
-
-filename='q2.1.html'
-soup = bs.BeautifulSoup(open(filename).read(), features="html.parser")
-functionalHeader = re.compile("(.*)Functional Description:(.*)")
-technicalHeader = re.compile("(.*)Technical Description:(.*)")
-
-rules = []
-
-for techDescHeaderTag in soup.findAll('p', text=technicalHeader):
-    ruleName = techDescHeaderTag.text.split()[0]
-    technical = findTextUntil(techDescHeaderTag, functionalHeader)
-    funcDescHeaderTag = findFuncHeaderTag(techDescHeaderTag, functionalHeader)
-    functional = findTextUntil(funcDescHeaderTag, technicalHeader)
-
-    rules.append( {
-        'name': ruleName,
-        'rules': {
-            'technical': removeHeader(technical),
-            'functional': removeHeader(functional)
-        }
-    } )
-
-# print(json.dumps(rules, indent=4, sort_keys=True))
-s = ''
-for rule in rules:
-    s = f"{s}\n##{rule['name']}"
-    s = f"{s}\n###Technical Rules"
-    s = f"{s}\n{rule['rules']['technical']}"
-    s = f"{s}\n###Functional Rules"
-    s = f"{s}\n{rule['rules']['functional']}"
-
-issueDate = getIssueDate(soup)
-documentVersion = getVersion(soup)
-
-d = f"---\ntitle: NCTS Phase 5 Technical Interface Specification\nweight: 5\ndescription: Software developers, designers, product owners or business analysts. Integrate your software with the ERMIS service\n---\n"
-d=d+f"#Rules\n"
-d=d+f"Based on document version {documentVersion} and issue date {issueDate}\n"
-d=d+f"<%= partial 'documentation/partials/rulesintro' %>\n{s}"
-with open("rules.html.md.erb", "w") as file:
-    file.write(d)
+with open('q2.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    with open("rules.html.md.erb", "w") as writer:
+        writer.write("""---
+title: NCTS Phase 5 Technical Interface Specification
+weight: 5
+description: Software developers, designers, product owners or business analysts. Integrate your software with the ERMIS service
+---
+#Rules
+Based on document version 5.15.0-v0.10 and issue date 04/03/2022
+<%= partial 'documentation/partials/rulesintro' %>
+""")
+        for row in reader:
+            writer.write(f"##{row[1]}\n\n")
+            writer.write(f"###Functional Description\n\n")
+            writer.write(f"{escape(row[2])}\n\n")
+            writer.write(f"###Technical Description\n\n")
+            writer.write(f"{escape(row[3])}\n\n")
