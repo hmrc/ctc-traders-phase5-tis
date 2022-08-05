eb5315c4d2dd38630bdabf6368cd95e8fa60a47f
Tim Squires <tim@crystalmark.co.uk>
2022-08-05 09:23:39 +0100

    CTCP-590 skip the header row in q2.csv

--- python/rules.py
+++ python/rules.py
@@ -14,8 +14,9 @@ Based on document version 5.15.0-v0.10 and issue date 04/03/2022
 <%= partial 'documentation/partials/rulesintro' %>
 """)
         for row in reader:
-            writer.write(f"##{row[1]}\n\n")
-            writer.write(f"###Functional Description\n\n")
-            writer.write(f"{escape(row[2])}\n\n")
-            writer.write(f"###Technical Description\n\n")
-            writer.write(f"{escape(row[3])}\n\n")
+            if row[0].isdigit():
+                writer.write(f"##{row[1]}\n\n")
+                writer.write(f"###Functional Description\n\n")
+                writer.write(f"{escape(row[2])}\n\n")
+                writer.write(f"###Technical Description\n\n")
+                writer.write(f"{escape(row[3])}\n\n")
