c0dc7f9ac056215ea99d8e96ae4fec91e569a7eb
Tim Squires <tim@crystalmark.co.uk>
2022-08-17 08:41:24 +0100

    CTCP-1075 filtered rules for traders

--- python/rules.py
+++ python/rules.py
@@ -6,7 +6,26 @@ import os
 def format(text):
     return text.replace('<', '&lt;').replace('>', '&gt;').replace('\n', '<br/>').replace('•', '- ')
 
-prefixes = ['b','c','e','g','r','s','t']
+prefixes = ['b','c','e','g','r','s']
+rules = []
+
+folder = os.getcwd()+'/../source/documentation/partials/'
+for filename in os.listdir(folder):
+    if filename.startswith("_IE") and filename.endswith(".md"):
+        soup = bs.BeautifulSoup(open(folder+filename).read(), features="html.parser")
+        # for a in soup.findAll('a', href=re.compile(r'rules.html#.*')):
+        for a in soup.findAll('a', href = re.compile(r'rules-.*')):
+            href = a['href']
+            rule = href.split('#')[1]
+            if rule not in rules:
+                rules.append(rule.upper())
+        #     href = href.replace('rules.html', f"rules-{rule[0]}.html")
+        #     a['href'] = href
+        #
+        # with open(filename, 'wt') as writer:
+        #     writer.write(str(soup))
+
+print(rules)
 
 for prefix in prefixes:
     with open(f"rules-{prefix}.html.md.erb", "wt") as writer:
@@ -25,22 +44,14 @@ with open('q2.csv', newline='') as csvfile:
     for row in reader:
         if row[0].isdigit():
             id = row[1]
-            prefix = id[0].lower()
-            with open(f"rules-{prefix}.html.md.erb", "at") as writer:
-                writer.write(f"##{id}\n\n")
-                writer.write(f"<b>Functional Description</b>\n\n")
-                writer.write(f"{format(row[2])}\n\n")
-                writer.write(f"<b>Technical Description</b>\n\n")
-                writer.write(f"{format(row[3])}\n\n")
+            print(id)
+            if id in rules:
+                print(id)
+                prefix = id[0].lower()
+                with open(f"rules-{prefix}.html.md.erb", "at") as writer:
+                    writer.write(f"##{id}\n\n")
+                    writer.write(f"<b>Functional Description</b>\n\n")
+                    writer.write(f"{format(row[2])}\n\n")
+                    writer.write(f"<b>Technical Description</b>\n\n")
+                    writer.write(f"{format(row[3])}\n\n")
 
-folder = os.getcwd()+'/../source/documentation/partials/'
-for filename in os.listdir(folder):
-    soup = bs.BeautifulSoup(open(folder+filename).read(), features="html.parser")
-    for a in soup.findAll('a', href = re.compile(r'rules.html#.*')):
-        href = a['href']
-        rule = href.split('#')[1]
-        href = href.replace('rules.html', f"rules-{rule[0]}.html")
-        a['href'] = href
-
-    with open(filename, 'wt') as writer:
-        writer.write(str(soup))
