e8a8d455d68a3a4b3728127ff37c7e26c7ff66e7
Sandeep Kumar Soni <112713042+sonisn1988@users.noreply.github.com>
2023-03-22 14:49:45 +0000

    Upgrade Service Guide Ruby version to 3.0.2

--- test/BuildSpec.scala
+++ test/BuildSpec.scala
@@ -15,12 +15,12 @@
  */
 
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpecLike
-import scala.language.postfixOps
+import org.scalatest.wordspec.AnyWordSpec
 
+import scala.language.postfixOps
 import scala.sys.process._
 
-class BuildSpec extends AnyWordSpecLike with Matchers {
+class BuildSpec extends AnyWordSpec with Matchers {
   "Building the content" should {
     "produce static files" in {
       val result = "bundle install" #&& Process("bundle exec middleman build --verbose --build-dir=public/ --clean", None, "BASE_PATH" -> "/guides/ctc-traders-phase5-tis/") !
